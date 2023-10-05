01da7e982534fd3bd3b78029b60b130b8b197761
athaka <jesoka18@gmail.com>
2023-10-05 19:03:31 +0100

    Hello with puts_1.1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-/* Print an output using 'puts' */
+/*
+   Print an output using 'puts' 
+   Return: 0 (Success) 
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
