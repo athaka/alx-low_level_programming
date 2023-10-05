14da296bea8f35ae19ea15b526558d7838d58a71
athaka <jesoka18@gmail.com>
2023-10-05 18:49:48 +0100

    Hello with puts_1.0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* Print an output using 'puts' */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("Programming is like building a multilingual puzzle,");
 return (0);
 }
