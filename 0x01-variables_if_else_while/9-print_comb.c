6d8bc383c42e385c19a34f48b8298579e7b4959d
athaka <jesoka18@gmail.com>
2023-10-10 17:28:28 +0100

    combining single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - A program that prints all possible combinations of
+  * single-digit numbers using putchar
+  * Return: 0 and exit
+  */
+int main(void)
+{
+	int n1, n2;
+
+	/* use a for loop to iterate from 0 to 15 */
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			putchar('0' + n1);
+			putchar(',');
+			putchar(' ');
+			putchar('0' + n2);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
