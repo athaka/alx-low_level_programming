78f8fb951439e9f31f9fafb2770b4811337fe4b8
athaka <jesoka18@gmail.com>
2023-10-10 14:32:16 +0100

    join together every single base 10 digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - A program that prints all single digits number of
+  * base 10 starting from zero (0)
+  * Return: 0 and exit
+  */
+int main(void)
+{
+	/* use a for loop to iterate from 0 to 9 */
+	for (int i = 0; i <= 9; i++)
+	{
+		/* print the current number followed by a new line */
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
