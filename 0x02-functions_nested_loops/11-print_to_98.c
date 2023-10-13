6e37be3d7b4053e4731343932d45e4bf4c779ae4
athaka <jesoka18@gmail.com>
2023-10-13 23:37:47 +0100

    print n to 98_1.0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,17 +8,29 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n <= 98)
 	{
-		printf("%d", n);
-		if (n != 98)
+		while (n <= 98)
 		{
-			printf(", ");
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
-		else
-		{
-			printf("\n");
-		}
-		n++;
 	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	printf("\n");
 }
