2135942b7ff0ebac9744fe94e2ac38c63c04ea8c
athaka <jesoka18@gmail.com>
2023-10-13 15:36:23 +0100

    print the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - A function that prints the absolute value of any number
+ * @n: integer to be transformed
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	{
+		n = n % 10;
+		return (n);
+	}
+}
