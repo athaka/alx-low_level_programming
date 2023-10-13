fd4eeb644fbbc484e40fc38f7e85a708eea620fc
athaka <jesoka18@gmail.com>
2023-10-13 19:21:55 +0100

    adding integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - A function that adds two numbers
+ * @a: - Any random integer
+ * @b: - Any random integer
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
