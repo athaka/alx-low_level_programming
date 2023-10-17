17e8c2baa07f1468421a76234586742c358075dd
athaka <jesoka18@gmail.com>
2023-10-17 23:23:38 +0100

    swaping values with * (dereference fxn)

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - set integer to 98
+ * @a: A pointer dereferencing a to 42
+ * @b: A pointer  dereferencing b to 98
+ * Return: nothing.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
