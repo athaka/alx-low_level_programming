496b39d07a8e81822114e3b4b3c3f76cab4111e1
athaka <jesoka18@gmail.com>
2023-10-26 18:03:16 +0100

    compare two strings_1.1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,70 +10,13 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
-	{
-		/* If s2 is empty, s1 must be empty for a match */
-		return (*s1 == '\0');
-	}
-	else if (*s2 == '*')
-	{
-		/* If s2 has a '*', try all possibilities of matching */
-		return (matchWild(s1, s2));
-	}
-	else if (*s1 == '\0')
-	{
-		/* If s1 is empty, s2 must be empty or contain only '*' for a match */
-		while (*s2 == '*')
-		{
-			s2++;
-		}
-		return ((*s2 == '\0'));
-	}
-	else if (match(s1, s2))
-	{
-		/* Regular characters match, move both pointers */
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	else
-	{
-		/* Characters don't match */
+	if (*s2 == '*' && *(s2 + 1) && !*s1)
 		return (0);
-	}
-}
-
-/**
- * match - A helper function for wildcmp
- * @s1: string 1
- * @s2: string 2
- *
- * Return: nothing
- */
-int match(char *s1, char *s2)
-{
-	return (*s1 == *s2 || *s2 == '.');
-}
-
-/**
- * matchWild - A helper function for wildcmp
- * @s1: string 1
- * @s2: string 2
- *
- * Return: nothing
- */
-int matchWild(char *s1, char *s2)
-{
-	if (*s2 == '\0')
-	{
-		/* A '*' at the end matches everything */
+	if (!*s1 && !*s2)
 		return (1);
-	}
-	while (*s1 != '\0')
-	{
-		if (wildcmp(s1, s2 + 1))
-		{
-			return (1);
-		}
-		s1++;
-	}
-	return (wildcmp(s1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
