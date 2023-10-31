9b18cf92ff45ce1554553ce1a8fd2e775771bb17
athaka <jesoka18@gmail.com>
2023-10-31 15:38:09 +0100

    high addition with main_1.1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -30,9 +30,9 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
-			sum += atoi(&argv[i][j]);
 			j++;
 		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
