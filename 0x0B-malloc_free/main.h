9eb463fbd66c93a11c196c9a2aa4cb67db3f7525
athaka <jesoka18@gmail.com>
2023-11-05 16:04:13 +0100

    malloc: split string

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,5 +10,8 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 void print_grid(int **grid, int width, int height);
 char *argstostr(int ac, char **av);
+void print_tab(char **tab);
+char **strtow(char *str);
+int countWords(char *str);
 
 #endif
