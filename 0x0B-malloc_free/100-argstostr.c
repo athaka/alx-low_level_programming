c024925fdbccb6bb9ed65e38f95c634685bb7f1d
athaka <jesoka18@gmail.com>
2023-11-05 01:46:58 +0100

    malloc: joins main argument

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr -  A function that concatenates the
+ * arguments provided to the program.
+ * @ac: Number of arguments.
+ * @av: Pointer to an array of arguments.
+ *
+ * Return: Nothing.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, l, pos;
+	char *concat;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	l = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		l += strlen(av[i]) + 1;
+	}
+	concat = (char *)malloc(l * sizeof(char));
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	pos = 0;
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(concat + pos, av[i]);
+		pos += strlen(av[i]);
+		concat[pos++] = '\n';
+	}
+	concat[pos] = '\0';
+
+	return (concat);
+}
