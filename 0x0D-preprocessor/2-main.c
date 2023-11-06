6dfc8a23b4156af38489b17cc44cd61b9086978f
athaka <jesoka18@gmail.com>
2023-11-06 16:53:40 +0100

    file print your name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A function that prints the name of this file
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
