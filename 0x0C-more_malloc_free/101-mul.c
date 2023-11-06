83672e97da5562531d8522c162f2c5a0e0e12d18
athaka <jesoka18@gmail.com>
2023-11-06 04:34:27 +0100

    multiply 2 +ve ints

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,13 +13,12 @@ int main(int argc, char *argv[])
 	int n1, n2, mul;
 
 	if (argc != 3)
-		printf("Error\n");
-	else
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		mul = n1 * n2;
-		printf("%d\n", mul);
+		printf("Error\n");
 	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	mul = n1 * n2;
+	printf("%d\n", mul);
 	return (0);
 }
