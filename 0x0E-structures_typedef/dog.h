568c1d38a53301b7757e3ee7ec963b341c93d92e
athaka <jesoka18@gmail.com>
2023-11-07 17:15:14 +0100

    my poppy bio

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure that declares my dog's profile
+ * @name: my dog's name
+ * @age: my dog's age
+ * @owner: me
+ * Return: 0 always
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
