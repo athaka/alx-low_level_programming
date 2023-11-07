ba0712b2e95481dd58dbdeb334ac991281860f79
athaka <jesoka18@gmail.com>
2023-11-07 22:22:17 +0100

    print my dog bio_1.2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,10 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+		printf("Name: ");
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+		printf("Owner: ");
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
 }
