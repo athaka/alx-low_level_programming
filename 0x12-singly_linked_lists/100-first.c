f98074238dd657af1d79c49395f7f3f34c1d021b
athaka <jesoka18@gmail.com>
2023-11-21 14:24:37 +0100

    doing startups

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * start_msg - Print a startup message before
+ * the main function is executed.
+ * Return: Nothing.
+ */
+
+void start_msg(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
