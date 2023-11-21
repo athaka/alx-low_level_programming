664e1042996d8499f7551c8f7469472c129cf22a
athaka <jesoka18@gmail.com>
2023-11-21 16:35:45 +0100

    in assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0   ; null-terminated string
-
-section .text
-    global main
-
-    extern printf
-
+          global    main
+          extern    printf
 main:
-    sub rsp, 8                       ; align the stack
-
-    mov rdi, hello                   ; format string
-    call printf                      ; call printf function
-
-    add rsp, 8                       ; restore the stack
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
