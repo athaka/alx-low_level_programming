f580f3f9903304fe940ac9ef289fb80240758936
athaka <jesoka18@gmail.com>
2023-11-22 14:41:33 +0100

    count elements in a  listint_t

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint_len - Return the number of elements in a listint_t list.
+ * listint_len - Return the number of elements in a listint_t list.
  * @h: A pointer to the head of the list_t list.
  *
  * Return: The number of nodes in the list.
